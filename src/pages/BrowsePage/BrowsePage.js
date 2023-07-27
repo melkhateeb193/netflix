0b44402c01e66ae130e5f69d5a8feb661317bf69
Mustafa Ali <engmostafa51999@gmail.com>
2023-07-27 16:16:22 +0300

    Last Adding by mostafa Routting

--- src/pages/BrowsePage/BrowsePage.js
+++ src/pages/BrowsePage/BrowsePage.js
@@ -1,23 +1,65 @@
    
 import NavBar from "../../NavBar/NavBar";
 import Browse from "../browseByLanguages/browse";
-import Footer from "../../footer/footer";
-import Card from "react-bootstrap/Card";
+import Footer from "../../footer/footer"; 
+import List from "../../list/list";
+
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import List from "../../list/list";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../AxiosConfig/fireBase";
+import { useEffect, useState } from "react";
+import ListItems from "../../listItems/listitems";
 export default function BrowsePage() {
- 
+  const [movies, setMovies] = useState([]);
+  const [moviesGenre, setMoviesGenres] = useState([[]]);
 
+  function getData(){
+    const resultsCollection=collection(db,"NetflixClone");
+    getDocs(resultsCollection).then(res=>{ 
+     const result =res.docs.map(doc=>({
+      data:doc.data(),
+      id:doc.id
+     }))
+     setMovies(result); 
+     setMoviesGenres(result.map(m=>m.data.Genres));
+     
+    }).catch(err=>{console.log(err);});
+  }
+  useEffect(() =>{
+    getData();
+
+  },[]);
+  // console.log(movies);
   return (
     <>
 
-      <NavBar></NavBar>
-      <Browse></Browse>
+        <NavBar></NavBar>  
+         <Browse></Browse> 
+
+      <div className='container-fluid ps-5'>
+      <div className="containerItemlist"> 
+    <Row xs={5} md={5} className="main-design   pt-4">
+      {movies.map((movie) => ( 
+        //  <Col key={movie.data.id} className="mb-4">
+           <Card className="h-100" style={{ width: "14rem" }}>
+           <ListItems key={ movie.id} index={movie.id} movie={movie.data} generss ={moviesGenre} />
+           </Card>
+            // </Col>
+ 
+      ))}
+    </Row>
+  </div>
+</div>
+
+      {/* <List></List>
       <List></List>
       <List></List>
-      <List></List>
-      <List></List>
+      <List></List> */}
       
       <Footer></Footer>
     </>
@@ -26,18 +68,3 @@ export default function BrowsePage() {
 }
 
 
-// <div className='container-fluid ps-5'>
-// <div className='list-body px-5 mt-4'>
-//     <Row xs={5} md={5} className="main-design g-2  pt-4">
-//       {movies.map((movie) => (
-//         <Col key={movie.id} className='  pb-5'>
-//           <Card className='card-design '>
-//             <Card.Img className='card-img ' variant="top" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}></Card.Img>
-
-//           </Card>
-
-//         </Col>
-//       ))}
-//     </Row>
-//   </div>
-// </div>
